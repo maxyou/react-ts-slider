38a88db7c8dbe512076bbec15b61b68016ae954b
maxyou <hyyou2010@gmail.com>
2020-09-25 01:23:57 +0800

    progress.tsx

--- src/view/carousel/index.tsx
+++ src/view/carousel/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
 import { keyframes } from 'styled-components'
 import Sub from './sub'
+import Progress from './progress'
 
 const StyledDivCarousel = styled.div`
     width: 100%;
@@ -39,40 +40,40 @@ const StyledDivRightArrow = styled.div`
     right:0%;
     background-color: blue;
 `
-const StyledDivProcess = styled.div`
-    width: 70%;
-    height: 10%;
-    position:absolute;
-    bottom:5%;
-    left:15%;
-    background-color: red;
-    display: flex;
-    justify-content: center;    
-    align-items: stretch;
-`
-const StyledDivProgressMax = styled.div`
-    width: 0%;
-    flex: 1 1 auto;
-    background-color: yellow;
-    margin:3px;    
-`
-const progress0to100 = keyframes`
-  from {
-    width: 0%;
-  }
-  to {
-    width: 100%;
-  }
-`
+// const StyledDivProcess = styled.div`
+//     width: 70%;
+//     height: 10%;
+//     position:absolute;
+//     bottom:5%;
+//     left:15%;
+//     background-color: red;
+//     display: flex;
+//     justify-content: center;    
+//     align-items: stretch;
+// `
+// const StyledDivProgressMax = styled.div`
+//     width: 0%;
+//     flex: 1 1 auto;
+//     background-color: yellow;
+//     margin:3px;    
+// `
+// const progress0to100 = keyframes`
+//   from {
+//     width: 0%;
+//   }
+//   to {
+//     width: 100%;
+//   }
+// `
 
-const StyledDivProgressValue = styled.div<{run:string}>`
-    width: 0%;
-    height: 100%;
-    background-color: green;
-    animation: ${progress0to100} 2s 1;
-    animation-fill-mode: forwards;
-    animation-play-state: ${props=>props.run};
-`
+// const StyledDivProgressValue = styled.div<{run:string}>`
+//     width: 0%;
+//     height: 100%;
+//     background-color: green;
+//     animation: ${progress0to100} 2s 1;
+//     animation-fill-mode: forwards;
+//     animation-play-state: ${props=>props.run};
+// `
 
 interface CarouselOption {
   slideTime: number,
@@ -88,10 +89,10 @@ function Carousel(props: IProps) {
   const childrenSub = React.Children.toArray(props.children).map((v, index)=><Sub key={index} subMsg='subMsg'>{v}</Sub>)
   
   const childrenNumber = childrenSub.length
-  const childrenProgress:any[] = []
+  // const childrenProgress:any[] = []
   const childrenLeft:number[] = []  
   // const [count, setCount] = useState(0)
-  const [runCtrl, setRunCtrl] = useState('paused')
+  // const [runCtrl, setRunCtrl] = useState('paused')
   const [currentSub, setCurrentSub] = useState(0) //1~n
 
   for(let i = 0; i < childrenNumber; i++){    
@@ -100,29 +101,29 @@ function Carousel(props: IProps) {
     
     childrenLeft.push(leftOffset)
     
-    childrenProgress.push(<StyledDivProgressMax key={i}>
-        <StyledDivProgressValue 
-          run={runCtrl}
-          onAnimationStart={onProgressAnimationStart} 
-          onAnimationEnd={onProgressAnimationEnd} 
-          id={`pv${i}`}>              
-        </StyledDivProgressValue>
-      </StyledDivProgressMax>)  
+    // childrenProgress.push(<StyledDivProgressMax key={i}>
+    //     <StyledDivProgressValue 
+    //       run={runCtrl}
+    //       onAnimationStart={onProgressAnimationStart} 
+    //       onAnimationEnd={onProgressAnimationEnd} 
+    //       id={`pv${i}`}>              
+    //     </StyledDivProgressValue>
+    //   </StyledDivProgressMax>)  
   }
 
   const [left, setLeft] = useState(childrenLeft[0])
 
 
-  function onProgressAnimationStart(e:any){
+  // function onProgressAnimationStart(e:any){
 
-    console.log(`onProgressAnimationStart: ${e.target.id}`)
+  //   console.log(`onProgressAnimationStart: ${e.target.id}`)
 
-  }
-  function onProgressAnimationEnd(e:any){
+  // }
+  // function onProgressAnimationEnd(e:any){
 
-    console.log(`onProgressAnimationEnd: ${e.target.id}`)
-    setLeft(childrenLeft[1])
-  }
+  //   console.log(`onProgressAnimationEnd: ${e.target.id}`)
+  //   setLeft(childrenLeft[1])
+  // }
 
 
   useEffect(
@@ -138,7 +139,7 @@ function Carousel(props: IProps) {
   function onArrowRightClicked(e:any){
     // alert('right')    
     console.log(e.target.id)
-    setRunCtrl('running')
+    // setRunCtrl('running')
   }
 
   return <StyledDivCarousel>
@@ -149,12 +150,9 @@ function Carousel(props: IProps) {
     </StyledDivWin>
     <StyledDivLeftArrow onClick={onArrowLeftClicked}  id='leftArrow'>{`<`}</StyledDivLeftArrow>
     <StyledDivRightArrow onClick={onArrowRightClicked} id='rightArrow'>{`>`}</StyledDivRightArrow>
-    <StyledDivProcess>
-      {
-        childrenProgress
-      }
-      {/* <StyledDivProgress></StyledDivProgress> */}
-    </StyledDivProcess>
+    <Progress childrenNum={childrenNumber} currentSub={1} callback={()=>{}}></Progress>
+    {/* <StyledDivProcess>
+    </StyledDivProcess> */}
   </StyledDivCarousel>
 }
 
---
+++ src/view/carousel/progress.tsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
+import { keyframes } from 'styled-components'
+
+const StyledDivProcess = styled.div`
+    width: 70%;
+    height: 10%;
+    position:absolute;
+    bottom:5%;
+    left:15%;
+    background-color: red;
+    display: flex;
+    justify-content: center;    
+    align-items: stretch;
+`
+const StyledDivProgressMax = styled.div`
+    width: 0%;
+    flex: 1 1 auto;
+    background-color: yellow;
+    margin:3px;    
+`
+const progress0to100 = keyframes`
+  from {
+    width: 0%;
+  }
+  to {
+    width: 100%;
+  }
+`
+
+const StyledDivProgressValue = styled.div<{ run: string }>`
+    width: 0%;
+    height: 100%;
+    background-color: green;
+    animation: ${progress0to100} 2s 1;
+    animation-fill-mode: forwards;
+    animation-play-state: ${props => props.run};
+`
+
+
+interface IProgress {
+  childrenNum: number,
+  currentSub: number,
+  callback: () => void
+}
+export default function Progress(props: IProgress) {
+  const [runCtrl, setRunCtrl] = useState('running')
+
+  const childrenProgress: any[] = []
+
+  for (let i = 0; i < props.childrenNum; i++) {
+    let leftOffset = ((props.childrenNum - 1) * 100) / 2 - (100 * i)
+    // console.log(`i=${i}, a=${leftOffset}`)
+
+
+    childrenProgress.push(<StyledDivProgressMax key={i}>
+      <StyledDivProgressValue
+        run={runCtrl}
+        onAnimationStart={onProgressAnimationStart}
+        onAnimationEnd={onProgressAnimationEnd}
+        id={`pv${i}`}>
+      </StyledDivProgressValue>
+    </StyledDivProgressMax>)
+  }
+
+
+
+  function onProgressAnimationStart(e: any) {
+
+    console.log(`onProgressAnimationStart: ${e.target.id}`)
+
+  }
+  function onProgressAnimationEnd(e: any) {
+
+    console.log(`onProgressAnimationEnd: ${e.target.id}`)
+    // setLeft(childrenLeft[1])
+  }
+
+  return <StyledDivProcess>
+    {childrenProgress}
+  </StyledDivProcess>
+
+
+}
